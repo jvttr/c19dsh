6a6e99f48c91ab43351cc4d11aaffc13545a1bd2
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2022-05-03 22:19:21 -0300

    Init project with Cookiecutter Data Science

---
+++ docs/index.rst
@@ -0,0 +1,24 @@
+.. c19dsh documentation master file, created by
+   sphinx-quickstart.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+c19dsh documentation!
+==============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   getting-started
+   commands
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project intends to be done for consolidate all knowledge that has been adquired from work at State Health Department. Also, I intend to do things that I idealized before but the desorganization from there place was an absurd. What ever, i wanna learn more while do some cool stuff.',
+    author='Vinícius de Lima Gonçalves',
+    license='MIT',
+)
