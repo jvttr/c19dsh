7a16e70ac9d5d3acff256458de0ec29760b83390
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2022-05-03 22:40:37 -0300

    initial modifications

--- README.md
+++ README.md
@@ -32,8 +32,8 @@ Project Organization
     │                         generated with `pip freeze > requirements.txt`
     │
     ├── setup.py           <- makes project pip installable (pip install -e .) so src can be imported
-    ├── src                <- Source code for use in this project.
-    │   ├── __init__.py    <- Makes src a Python module
+    ├── c19dsh             <- Source code for use in this project.
+    │   ├── __init__.py    <- Makes c19dsh a Python module
     │   │
     │   ├── data           <- Scripts to download or generate data
     │   │   └── make_dataset.py
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='c19dsh',
     packages=find_packages(),
     version='0.1.0',
-    description='This project intends to be done for consolidate all knowledge that has been adquired from work at State Health Department. Also, I intend to do things that I idealized before but the desorganization from there place was an absurd. What ever, i wanna learn more while do some cool stuff.',
+    description='This project intends to be done for consolidate all knowledge that has been acquired from work at State Health Department. Also, I intend to do things that I idealized before but the disorganization from there place was an absurd. What ever, i wanna learn more while do some cool stuff.',
     author='Vinícius de Lima Gonçalves',
     license='MIT',
 )
